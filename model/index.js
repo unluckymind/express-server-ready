9760d80a08308a27e1e9b6c3b1cdaa2499d32822
dea <dea.aprizal@icloud.com>
2019-07-22 14:30:10 +0700

    updated routes

--- model/index.js
+++ model/index.js
@@ -5,3 +5,7 @@ const response = require("../config/payload_config");
 exports.index = (req, res) => {
     response.ok({ data: "API v1 Ready!" }, res)
 };
+
+exports.main = (req, res) => {
+    response.ok({ data: ["current RESTful API version: sahabat-halosis-v1", "requirement: authentic bearer code"] }, res)
+};
