cc074ff37abd73fca7c8629327fd490002567341
dea <dea.aprizal@icloud.com>
2019-07-23 16:35:24 +0700

    update profil detail + update profile password

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,26 @@
 const mysql = require("mysql");
 
 let users = ''
-let passwords = 'PJYwD5uvR3sXWCG8h'
+let passwords = ''
 let databases = ''
-let hosts = 'localhost'
-let ports = '3306'
 
 if (process.env.NODE_ENV !== 'production') {
   users = 'root'
   passwords = ''
-  database = 'sahabathalosis'
+  databases = 'sahabathalosis'
 } else {
   users = 'admin'
   passwords = 'PJYwD5uvR3sXWCG8h'
-  database = 'sahabat_db'
+  databases = 'sahabat_db'
 }
 
 const con = mysql.createConnection({
-  host: hosts,
+  host: 'localhost',
   user: users,
   password: passwords,
   database: databases,
-  port: ports
+  port: '3306'
 });
-
-console.log('user:' + users);
 
 con.connect((err) => {
   err ? console.log(err) : console.log("database connected, current env is on " + process.env.NODE_ENV)
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,15 @@ module.exports = function (app) {
   const modelToken = require("../model/webtoken");
 
   app.route("/").get(model.main);
-  app.route("/v1/").get(model.index);
+  app.route("/v1").get(model.index);
   app.route("/v1/members").get(modelMember.index);
   app.route("/v1/members/:id").get(modelMember.id);
 
   app.route("/v1/members").post(modelMember.register);
   app.route("/v1/members/dashboard").post(modelMember.login);
 
+  app.route("/v1/members").put(modelMember.update);
+  app.route("/v1/members/password").put(modelMember.updatePassword);
+
   app.route("/v1/tokens/apikey").get(modelToken.apikey);
-  app.route("/v1/tokens/test").get(modelToken.index);
 }
