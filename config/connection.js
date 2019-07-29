b81574aae487d8722fb0eb87a6e6064ef493b53b
dea <dea.aprizal@icloud.com>
2019-07-29 08:15:40 +0700

    mysql dynamic connection configuration

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,21 @@ const mysql = require("mysql");
 
 let users = ''
 let passwords = ''
+let hosts = ''
 let databases = 'sahabat_db'
 
 if (process.env.NODE_ENV !== 'production') {
   users = 'root'
+  hosts = 'localhost'
   passwords = ''
 } else {
   users = 'admin'
+  hosts = '35.197.148.245'
   passwords = 'PJYwD5uvR3sXWCG8h'
 }
 
 const con = mysql.createConnection({
-  host: '35.197.148.245',
+  host: hosts,
   user: users,
   password: passwords,
   database: databases,
