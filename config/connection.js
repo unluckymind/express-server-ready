2f4ec8bcc64952a228ce62bb25842e5cd02558ca
dea <dea.aprizal@icloud.com>
2019-07-29 15:38:45 +0700

    fixing connection + cron job API

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,20 @@ const mysql = require("mysql");
 let users = ''
 let passwords = ''
 let databases = 'sahabat_db'
+let hosts = ''
 
 if (process.env.NODE_ENV !== 'production') {
   users = 'root'
+  hosts = 'localhost'
   passwords = ''
 } else {
-  users = 'admin'
+  users = 'admin',
+    hosts = '35.197.148.245'
   passwords = 'PJYwD5uvR3sXWCG8h'
 }
 
 const con = mysql.createConnection({
-  host: '35.197.148.245',
+  host: hosts,
   user: users,
   password: passwords,
   database: databases,
